0bb36a1556f56c8622d0d51abed8fdab26ad789d
Shashi-1code <sashidarkamanur@gmail.com>
2025-11-24 10:53:51 +0530

    added admin introo

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import {addProduct, listProduct, removeProduct, singleProduct} from '../controllers/productController.js';
 import upload from '../middleware/multer.js';
+import adminAuth from '../middleware/adminAuth.js';
 const productRouter= express.Router();
 
-productRouter.post('/add',upload.fields([{name:'image1',maxcount:1},{name:'image2',maxcount:1},{name:'image3',maxcount:1},{name:'image4',maxcount:1}]), addProduct);
+productRouter.post('/add',adminAuth,upload.fields([{name:'image1',maxcount:1},{name:'image2',maxcount:1},{name:'image3',maxcount:1},{name:'image4',maxcount:1}]), addProduct);
 productRouter.get('/list', listProduct);
-productRouter.post('/remove', removeProduct);
+productRouter.post('/remove',adminAuth, removeProduct);
 productRouter.post('/single', singleProduct);
 
 export default productRouter;
